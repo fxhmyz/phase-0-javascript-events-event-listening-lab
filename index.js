499e00967255920c5244d5772a37bcea82a2898f
fxhmyz <alifahmy99111@gmail.com>
2024-07-18 16:45:44 +0300

    first initial commit

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
 function addingEventListener() {
 }
+// Function to add event listener to an element
+function addingEventListener() {
+    // Get the element with id 'button'
+    const input = document.getElementById('button');
+    
+    // Define the callback function
+    function clickAlert() {
+      alert('I was clicked!');
+    }
+  
+    // Add the event listener
+    input.addEventListener('click', clickAlert);
+  }
+  
+  // Call the function to add the event listener
+  addingEventListener();
+  
